461e0d50147a0ff1b0dcc97ca0bc8be176f61c09
phi-line <phi.liney@gmail.com>
2018-01-05 01:46:41 -0800

    added inline facts

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
+
 import requests
 FACT_URL = "https://catfact.ninja/fact"
 
@@ -16,12 +19,32 @@ def getApiKey():
     with open('TOKEN.txt') as f:
         return f.readline()
 
-def fact(bot, update):
+def messageFact(bot, update):
     text = update.message.text
     text = text.lower()
     if "cat" in text and "fact" in text:
         cat_fact = fetchFact()
         cat_fact and update.message.reply_text(cat_fact)
+
+def inlineFact(bot, update):
+    query = update.inline_query.query
+
+    if not query:
+        return
+
+    cat_fact = fetchFact()
+
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Cat Fact',
+            input_message_content=InputTextMessageContent(cat_fact)
+        )
+    )
+
+    bot.answer_inline_query(update.inline_query.id, results)
+
 
 
 def error(bot, update, error):
@@ -37,7 +60,11 @@ def main():
     dp = updater.dispatcher
 
     # message listener for catfacts
-    dp.add_handler(MessageHandler(Filters.text, fact))
+    dp.add_handler(MessageHandler(Filters.text, messageFact))
+
+    # inline listener for catfacts
+    dp.add_handler(InlineQueryHandler(inlineFact))
+    dp.add_handler(MessageHandler(Filters.text, messageFact))
 
     # log all errors
     dp.add_error_handler(error)
