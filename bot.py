230e2143a3e911e7029e288a2f067bb4728ba2d5
phi-line <phi.liney@gmail.com>
2018-01-04 17:35:32 -0800

    cleaned up code

--- bot.py
+++ bot.py
@@ -1,50 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Simple Bot to reply to Telegram messages.
-
-This program is dedicated to the public domain under the CC0 license.
-
-This Bot uses the Updater class to handle the bot.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 import requests
 FACT_URL = "https://catfact.ninja/fact"
-TOKEN= ""
 
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+def getApiKey():
+    with open('TOKEN.txt') as f:
+        return f.readline()
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(bot, update):
-    """Echo the user message."""
+def fact(bot, update):
     text = update.message.text
     text = text.lower()
     if "cat" in text and "fact" in text:
@@ -53,40 +25,31 @@ def echo(bot, update):
 
 
 def error(bot, update, error):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
-    """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater(TOKEN)
+    key = getApiKey()
+    updater = Updater(key)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # message listener for catfacts
+    dp.add_handler(MessageHandler(Filters.text, fact))
 
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
+    # start bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 def fetchFact():
     r = requests.get(FACT_URL)
     json = r.json()
-    print(json)
     return json['fact']
 
 
